302f4ec53780920c78a0adb03b37d1e141c29126
Dillon Ellis <dillonzellis@gmail.com>
2022-04-01 11:52:51 -0400

    desktop submenu styling and functionality

--- src/js/mobile-dropdown.js
+++ src/js/mobile-dropdown.js
@@ -2,10 +2,12 @@ const mobileDropdown = () => {
   const accent = document.querySelector(".accent");
   const dropDown = document.querySelector(".submenu");
   const searchBar = document.querySelector(".search-bar");
+  const hr = document.querySelector("hr");
 
   accent.addEventListener("click", () => {
     dropDown.classList.toggle("show-block");
     searchBar.classList.remove("show-flex");
+    hr.classList.toggle("show-block");
   });
 };
 
--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -2,10 +2,12 @@ const searchBarDisplay = () => {
   const searchIcon = document.querySelector(".search");
   const searchBar = document.querySelector(".search-bar");
   const subMenu = document.querySelector(".submenu");
+  const hr = document.querySelector("hr");
 
   searchIcon.addEventListener("click", () => {
     searchBar.classList.toggle("show-flex");
     subMenu.classList.remove("show-block");
+    hr.classList.remove("show-block");
   });
 };
 

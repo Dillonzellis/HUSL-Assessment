b759afc2221501b7c8553a533dfa383ce7d2e8b9
Dillon Ellis <dillonzellis@gmail.com>
2022-03-31 21:56:13 -0400

    desktop search bar toggle

--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -1,9 +1,11 @@
 const searchBarDisplay = () => {
   const searchIcon = document.querySelector(".search");
   const searchBar = document.querySelector(".search-bar");
+  const subMenu = document.querySelector(".submenu");
 
   searchIcon.addEventListener("click", () => {
     searchBar.classList.toggle("show-flex");
+    subMenu.classList.remove("show-block");
     console.log("searchbar");
   });
 };
